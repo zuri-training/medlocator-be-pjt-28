8acd3c81b9b452e735b9572f51b6ca9983a75dc8
emekarr <aceanyanwu02@gmail.com>
2021-06-28 14:25:41 +0100

    edited the routes index.js to catch requests made to unknown routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,5 +8,13 @@ router.use("/api/v1/user", userRoutes);
 router.use("/api/v1/store", storeRoutes);
 router.use("/api/v1/drug", drugRoute);
 router.use("/", homeRoutes);
+router.use("*", (req, res) => {
+  res.status(404).json({
+    status: "failure",
+    code: 404,
+    message: "This page does not exist",
+    body: {},
+  });
+});
 
 module.exports = router;
