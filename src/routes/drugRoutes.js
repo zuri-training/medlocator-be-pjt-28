cd479e3e53bb813b6ed8bb821475018bc634f01f
emekarr <aceanyanwu02@gmail.com>
2021-06-29 13:48:53 +0100

    added the protect function from auth controller to my routes for authentication

--- src/routes/drugRoutes.js
+++ src/routes/drugRoutes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const respondJSON = require("../controllers/responseHandler");
+const { protect } = require("../controllers/authController");
 const {
   add_drug,
   toogle_availability,
@@ -10,11 +11,11 @@ const {
 } = require("../controllers/drugController");
 const router = Router();
 
-router.post("/create", add_drug, respondJSON);
-router.put("/available", toogle_availability, respondJSON);
-router.delete("/delete/:drug_name", delete_drug, respondJSON);
-router.get("/:drug_name", get_drug, respondJSON);
-router.get("/", get_drugs, respondJSON);
-router.put("/update/:drug_id", update_drugs, respondJSON);
+router.post("/create", protect, add_drug, respondJSON);
+router.put("/available", protect, toogle_availability, respondJSON);
+router.delete("/delete/:drug_name", protect, delete_drug, respondJSON);
+router.get("/:drug_name", protect, get_drug, respondJSON);
+router.get("/", protect, get_drugs, respondJSON);
+router.put("/update/:drug_id", protect, update_drugs, respondJSON);
 
 module.exports = router;
